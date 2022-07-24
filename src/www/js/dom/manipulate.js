a693b5d9ce732cb9589e93a3f2fad36491eb3e93
Andrew Smith <andrewsmith@alumni.stanford.edu>
2022-07-24 13:41:52 -0700

    Clean up manipulate demo.

--- src/www/js/dom/manipulate.js
+++ src/www/js/dom/manipulate.js
@@ -1,9 +1,7 @@
-const bar = () => {
-  // <<: text
+const addText = () => {
   const message = document.createElement('p')
   message.appendChild(document.createTextNode('Hello'))
   document.body.appendChild(message)
-  // :>>
 }
 
 const addImage = () => {
@@ -14,14 +12,6 @@ const addImage = () => {
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max))
-}
-
-const foo = () => {
-  // <<: img
-  const image = document.createElement('img')
-  image.src = 'https://www.placecage.com/200/300'
-  document.getElementById('root').appendChild(image)
-  // :>>
 }
 
 const addList = () => {
@@ -39,65 +29,33 @@ const addList = () => {
   document.getElementById('root').appendChild(list)
 }
 
-// for slides, don't use in exercise
-const shortList = () => {
-  // <<: list
-  const list = document.createElement('ul')
-
-  const todo1 = document.createElement('li')
-  todo1.appendChild(document.createTextNode('Learn DOM'))
-
-  list.appendChild(todo1)
-
-  document.getElementById('root').appendChild(list)
-  // :>>
-}
-
-// <<: re-append
 const moveToEnd = () => {
   const list = document.getElementsByTagName('ul')[0]
   const todo = list.children[0]
   list.appendChild(todo)
 }
-// :>>
 
 const setHtml = () => {
-  // <<: inner-html
   document.body.innerHTML = '' +
     '<ul>' +
     '  <li>Learn DOM</li>' +
     '  <li>Practice innerHMTL</li>' +
     '</ul>'
-
-  console.log(document.body.innerHTML)
-  // :>>
 }
 
 const setText = () => {
-  // <<: inner-text
   const title = document.getElementById('title')
-  console.log(title.innerText) // 'Welcome'
   title.innerText = 'Goodbye'
-  console.log(title.innerText) // 'Goodbye'
-  // :>>
 }
 
-const formInput = () => {
-  // <<: form-value
+const addFormInput = () => {
   const input = document.createElement('input')
   input.value = 'I love tea'
-  console.log(input.value) // 'I love tea'
   document.body.appendChild(input)
-  // :>>
 }
 
 const toggleVisibility = () => {
-  // <<: class-toggle
   const title = document.getElementById('title')
-  title.classList.contains('hidden') // false
   title.classList.toggle('hidden')
-  title.classList.contains('hidden') // true
-  title.classList.toggle('hidden')
-  title.classList.contains('hidden') // false
-  // :>> class
+  console.log('Is the title now hidden?', title.classList.contains('hidden'))
 }
